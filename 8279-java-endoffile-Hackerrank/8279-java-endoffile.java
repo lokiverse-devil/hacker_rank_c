f7f37497036e2353c8e0e7e1eaa7d0d8000a7da8
LOKI <ompandey2341@gmail.com>
2026-07-24 14:53:15 +0530

    Force committed solution - Time: O(n), Space: O(1)

---
+++ 8279-java-endoffile-Hackerrank/8279-java-endoffile.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner consoleScanner = new Scanner(System.in);
+ }
+        int n=1;
+
+}
+while (consoleScanner.hasNext()) {
+    String line= consoleScanner.nextLine();
+
+    System.out.println(n+" "+ line);
+    n++;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
